c1aa348f377f434b7f4968da4327452960d0ace2
sara <sara.ahumidi4@gmail.com>
2023-06-05 18:06:24 +0300

    Initial Commit

---
+++ datasetAi/datasetAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  datasetAiApp.swift
+//  datasetAi
+//
+//  Created by Sara Alhumidi on 16/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct datasetAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
